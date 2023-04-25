2b72a4ee95de9aec34741f7a67a07e2b0035e3d7
Geert Bevin <gbevin@uwyn.com>
2023-04-25 07:37:22 -0400

    Updated for RIFE2 and bld 1.6.1

--- src/bld/java/pokemon/PokemonBuild.java
+++ src/bld/java/pokemon/PokemonBuild.java
@@ -20,39 +20,28 @@ public class PokemonBuild extends WebProject {
         uberJarMainClass = "pokemon.PokemonSiteUber";
         version = version(0,1,0);
 
-        precompiledTemplateTypes = List.of(HTML);
-
         downloadSources = true;
+        autoDownloadPurge = true;
+
         repositories = List.of(MAVEN_CENTRAL, SONATYPE_SNAPSHOTS);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "rife2", version(1,5,6)))
+            .include(dependency("com.uwyn.rife2", "rife2", version(1,6,1)))
             .include(dependency("dev.mccue", "json", version(0,2,3)));
         scope(test)
             .include(dependency("org.jsoup", "jsoup", version(1,15,4)))
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,9,2)))
             .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,9,2)));
         scope(standalone)
-            .include(dependency("org.eclipse.jetty", "jetty-server", version(11,0,14)))
-            .include(dependency("org.eclipse.jetty", "jetty-servlet", version(11,0,14)))
+            .include(dependency("org.eclipse.jetty", "jetty-server", version(11,0,15)))
+            .include(dependency("org.eclipse.jetty", "jetty-servlet", version(11,0,15)))
             .include(dependency("org.slf4j", "slf4j-simple", version(2,0,7)));
+
+        precompileOperation()
+            .templateTypes(HTML);
     }
 
-    public static class TailwindWatchHelp implements CommandHelp {
-        @Override
-        public String getSummary() {
-            return "Runs the tailwind build in watch mode";
-        }
-    }
-
-    public static class TailwindHelp implements CommandHelp {
-        @Override
-        public String getSummary() {
-            return "Runs the tailwind build once";
-        }
-    }
-
-    @BuildCommand(help = TailwindWatchHelp.class)
-    public void tailwind_watch() throws IOException, InterruptedException, ExitStatusException {
+    @BuildCommand(summary = "Runs the tailwind build in watch mode")
+    public void tailwind_watch() throws Exception {
         int status = new ProcessBuilder(
                 "npx",
                 "tailwindcss",
@@ -71,8 +60,8 @@ public class PokemonBuild extends WebProject {
         }
     }
 
-    @BuildCommand(help = TailwindHelp.class)
-    public void tailwind() throws IOException, InterruptedException, ExitStatusException {
+    @BuildCommand(summary = "Runs the tailwind build once")
+    public void tailwind() throws Exception {
         int status = new ProcessBuilder(
                 "npx",
                 "tailwindcss",
